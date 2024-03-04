b4348b127ef68a02ae90bb63b96cc16e54dc7ca5
Natalya <117934761+Adsorbchia@users.noreply.github.com>
2024-03-05 00:55:11 +0400

    added a decorator login_required

--- my_book_re/my_book_re/settings.py
+++ my_book_re/my_book_re/settings.py
@@ -11,8 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from re import L
 
-from django.conf.global_settings import AUTH_USER_MODEL, INTERNAL_IPS
+from django.conf.global_settings import AUTH_USER_MODEL, INTERNAL_IPS, LOGIN_URL
 
 
 
@@ -151,3 +152,4 @@ INTERNAL_IPS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
+LOGIN_URL = '/user/login/'
